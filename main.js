e16024daa13a466cee27f7f963583d5e5938926f
JoshLDowns <53030592+JoshLDowns@users.noreply.github.com>
2019-11-02 10:46:47 -0400

    timer and random box working properly, currently declares draw if you win on last turn

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let playerArrayX = [];
 let playerArrayO = [];
 let compArray = [];
 let turnCount = 0;
-let trueCountX = 0; 
+let trueCountX = 0;
 let trueCountO = 0;
 let mustBlock = false;
 let blockArray = [];
@@ -11,24 +11,28 @@ let currentBox;
 let playerXName = 'X';
 let playerOName = 'O';
 let currentPlayer = 'X';
+let timerClick = true;
+let timer;
+let count = 6;
 
 let boxes = Array.from(document.getElementsByClassName("box"));
 
 class Box {
-    constructor(element) {
+    constructor(element, value) {
         this.element = element;
+        this.value = value;
         this.clicked = false;
     }
 }
-let one = new Box(document.getElementById("one"));
-let two = new Box(document.getElementById("two"));
-let three = new Box(document.getElementById("three"));
-let four = new Box(document.getElementById("four"));
-let five = new Box(document.getElementById("five"));
-let six = new Box(document.getElementById("six"));
-let seven = new Box(document.getElementById("seven"));
-let eight = new Box(document.getElementById("eight"));
-let nine = new Box(document.getElementById("nine"));
+let one = new Box(document.getElementById("one"),1);
+let two = new Box(document.getElementById("two"),2);
+let three = new Box(document.getElementById("three"),3);
+let four = new Box(document.getElementById("four"),4);
+let five = new Box(document.getElementById("five"),5);
+let six = new Box(document.getElementById("six"),6);
+let seven = new Box(document.getElementById("seven"),7);
+let eight = new Box(document.getElementById("eight"),8);
+let nine = new Box(document.getElementById("nine"),9);
 
 boxArray = [one, two, three, four, five, six, seven, eight, nine];
 
@@ -45,15 +49,15 @@ let boxIDLookUp = {
 }
 
 let blockBoxLookUp = {
-    '1' : one,
-    '2' : two,
-    '3' : three,
-    '4' : four,
-    '5' : five,
-    '6' : six,
-    '7' : seven,
-    '8' : eight,
-    '9' : nine
+    '1': one,
+    '2': two,
+    '3': three,
+    '4': four,
+    '5': five,
+    '6': six,
+    '7': seven,
+    '8': eight,
+    '9': nine
 }
 
 let boxNumLookUp = {
@@ -76,7 +80,7 @@ subOne.addEventListener("submit", event => {
     event.preventDefault();
     playerXName = (event.target.querySelector('input').value).toString();
     subOneButton.disabled = true;
-    })
+})
 
 let subTwo = document.getElementById("subTwo");
 let subTwoButton = document.getElementById("sub2");
@@ -84,7 +88,7 @@ subTwo.addEventListener("submit", event => {
     event.preventDefault();
     playerOName = (event.target.querySelector('input').value);
     subTwoButton.disabled = true;
-    })
+})
 
 let textDisplay = document.getElementById("textDisplay");
 let onePlayerGame = document.getElementById("start1p");
@@ -93,6 +97,7 @@ let twoPlayerGame = document.getElementById("start2p")
 twoPlayerGame.addEventListener("click", () => {
     twoPlayerGame.disabled = true;
     textDisplay.textContent = `It is ${playerXName}'s turn!`
+    timer = setInterval(countDown, 1000);;
     playerArrayO = [];
     playerArrayX = [];
     turnCount = 0;
@@ -109,16 +114,17 @@ function boxClick() {
     let boxNum = event.target.id;
     currentBox = boxIDLookUp[boxNum];
     if (playerTurn === true && currentBox.clicked === false) {
+        event.target.style.color = 'black';
         event.target.textContent = currentPlayer;
         currentBox.clicked = true;
         if (currentPlayer === 'X') {
             playerArrayX.push(boxNumLookUp[boxNum]);
             playerXClick();
+
         } else {
             playerArrayO.push(boxNumLookUp[boxNum]);
             playerOClick();
         }
-        //event.target.removeEventListener("click", boxClick);
         turnCount += 1;
         if (turnCount === 9) {
             for (box of boxes) {
@@ -127,6 +133,7 @@ function boxClick() {
             subOneButton.disabled = false;
             subTwoButton.disabled = false;
             twoPlayerGame.disabled = false;
+            clearInterval(timer);
             return textDisplay.textContent = `OH NO! It's a Draw!!!!`;
         }
     } else {
@@ -142,8 +149,8 @@ function playerXClick() {
                 if (arr.includes(playerArrayX[i])) {
                     trueCountX = trueCountX + 1;
                     //if (trueCountX === 2) {
-                        //mustBlock = true;
-                        //blockArray.push(arr);
+                    //mustBlock = true;
+                    //blockArray.push(arr);
                     //}
                     if (trueCountX === 3) {
                         for (box of boxes) {
@@ -152,22 +159,39 @@ function playerXClick() {
                         subOneButton.disabled = false;
                         subTwoButton.disabled = false;
                         twoPlayerGame.disabled = false;
+                        clearInterval(timer);
+                        count = 6;
+                        for (obj of boxArray) {
+                            if (obj.clicked === false) {
+                                obj.element.textContent = ``;
+                            }
+                        }
                         return textDisplay.textContent = `${playerXName} WINS!!!!`;
                     }
                 }
             }
-            //if (trueCountX === 3) {
-            //    textDisplay.textContent = `Player X WINS!!!!`;
-            //    for (box of boxes) {
-            //        box.removeEventListener("click", boxClick);
-            //    }
-            //}
         }
         currentPlayer = 'O';
+        clearInterval(timer);
+        count = 6;
+        for (obj of boxArray) {
+            if (obj.clicked === false) {
+                obj.element.textContent = ``;
+            }
+        }
+        timer = setInterval(countDown, 1000);
         return textDisplay.textContent = `It is ${playerOName}'s turn!`;
     } else {
-    currentPlayer = 'O';
-    return textDisplay.textContent = `It is ${playerOName}'s turn!`;
+        currentPlayer = 'O';
+        clearInterval(timer);
+        count = 6;
+        for (obj of boxArray) {
+            if (obj.clicked === false) {
+                obj.element.textContent = ``;
+            }
+        }
+        timer = setInterval(countDown, 1000);
+        return textDisplay.textContent = `It is ${playerOName}'s turn!`;
     }
 }
 
@@ -186,18 +210,88 @@ function playerOClick() {
                     subOneButton.disabled = false;
                     subTwoButton.disabled = false;
                     twoPlayerGame.disabled = false;
+                    clearInterval(timer);
+                    count = 6;
+                    for (obj of boxArray) {
+                        if (obj.clicked === false) {
+                            obj.element.textContent = ``;
+                        }
+                    }
                     return textDisplay.textContent = `${playerOName} WINS!!!!`;
                 }
             }
         }
         currentPlayer = 'X';
+        clearInterval(timer);
+        count = 6;
+        for (obj of boxArray) {
+            if (obj.clicked === false) {
+                obj.element.textContent = ``;
+            }
+        }
+        timer = setInterval(countDown, 1000);
         return textDisplay.textContent = `It is ${playerXName}'s turn!`;
     } else {
-    currentPlayer = 'X';
-    return textDisplay.textContent = `It is ${playerXName}'s turn!`;
+        currentPlayer = 'X';
+        clearInterval(timer);
+        count = 6;
+        for (obj of boxArray) {
+            if (obj.clicked === false) {
+                obj.element.textContent = ``;
+            }
+        }
+        timer = setInterval(countDown, 1000);
+        return textDisplay.textContent = `It is ${playerXName}'s turn!`;
     }
 }
 
+function ranNoClickBox() {
+    let noClick = [];
+    for (obj of boxArray) {
+        if (obj.clicked === false) {
+            noClick.push(obj)
+        }
+    }
+    let index = (Math.floor(Math.random() * noClick.length + 1) - 1);
+    noClick[index].clicked = true;
+    noClick[index].element.textContent = currentPlayer;
+    noClick[index].element.style.color = 'navy';
+    if (turnCount === 9) {
+        for (box of boxes) {
+            box.removeEventListener("click", boxClick);
+        }
+        subOneButton.disabled = false;
+        subTwoButton.disabled = false;
+        twoPlayerGame.disabled = false;
+        clearInterval(timer);
+        count = 6;
+        return textDisplay.textContent = `OH NO! It's a Draw!!!!`;
+    }
+    if (currentPlayer === 'X') {
+        turnCount += 1;
+        playerArrayX.push(noClick[index].value);
+        console.log(playerArrayX)
+            playerXClick();
+    } else {
+        turnCount += 1;
+        playerArrayO.push(noClick[index].value);
+            playerOClick();
+    }
+}
+
+function countDown() {
+    if (count === 0) {
+        ranNoClickBox();
+        clearInterval(countDown);
+    } else {
+        count = count - 1;
+        for (obj of boxArray) {
+            if (obj.clicked === false) {
+                obj.element.textContent = `${count}`;
+            }
+        }
+    }
+}
 //function compClick() {
 //    if (mustBlock === true) {
 //        for (element of blockArray) {
